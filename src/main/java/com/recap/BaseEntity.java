c82aff73cbbaaf00e0770dbba4dfe5f8aee69b79
Andrew <a.b.paliychuk@gmail.com>
2019-05-01 00:13:17 +0300

    Created new class BaseEntity

---
+++ src/main/java/com/recap/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.recap;
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
